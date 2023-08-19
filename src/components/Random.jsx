4e8b0fecb8c84f5cd19cedbf0466a7300a084219
swetagit186 <sweta.kumari.eee22@itbhu.ac.in>
2023-08-19 19:21:37 +0530

    commit-3

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -11,10 +11,13 @@ const Random = () => {
     <div className="w-1/2 bg-green-500 rounded-lg border-black flex flex-col items-center gap-y-5 mt-[15px]">
       
       <h1 className="mt-[15px] text-2xl underline uppercase font-bold">
-
+      A Random Gif
       </h1>
+      {
+        loading?(<Spinner/>):(<img src={gif} width='450'></img>)
+      }
       <button>
-
+      
       </button>
     </div>
   )
